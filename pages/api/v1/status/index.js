67f0d89f2e0fda9bc46a9ecf4bcf722d449cb43e
Lucas Leonel Nunes <106168937+Lucas-Leonel@users.noreply.github.com>
2025-10-23 02:18:19 +0000

    Adicionando status `updated_at`, `version`, `maxconnections` e `activeConnections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 3 + 3 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Status" });
+  const updatedAt = new Date().toISOString();
+
+  const dbversion = await database.query(
+    `SELECT split_part(version(), ',', 1) AS version;`,
+  );
+  const maxconnections = await database.query("SHOW max_connections;");
+  const activeConnections = await database.query(
+    `SELECT count(*) FROM pg_stat_activity;`,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: dbversion.rows[0].version,
+    maxconnections: maxconnections.rows[0].max_connections,
+    activeConnections: activeConnections.rows[0].count,
+  });
 }
 
 export default status;
--- teste/integration/api/v1/status/get.test.js
+++ teste/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
+const { version } = require("react");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Teste validando que a data existe e está no formato ISO
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  //Validando versão do Postgres
+  const versionPostgres = responseBody.version;
+  expect(versionPostgres).toEqual("PostgreSQL 16.0 on x86_64-pc-linux-musl");
+  expect(versionPostgres).not.toBeNull();
+
+  //Validando quantidade maxima de conexões ao banco
+  const maxconnections = responseBody.maxconnections;
+  expect(maxconnections).toEqual("100");
+  expect(maxconnections).not.toBeNull;
+
+  //Valida conexão existente
+  expect(responseBody.activeConnections).toBeDefined();
+  expect(Number(responseBody.activeConnections)).toBeGreaterThan(0);
 });
