18961d9dc08c3c6066c1f4a85b2c8331e0303e56
Lucas Leonel Nunes <106168937+Lucas-Leonel@users.noreply.github.com>
2025-12-05 02:01:18 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,28 +4,32 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  // Teste validando que a data existe e está no formato ISO
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      // Teste validando que a data existe e está no formato ISO
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+      const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  //Validando versão do Postgres
-  const versionPostgres = responseBody.dependencies.version;
-  expect(versionPostgres).toEqual("16.0");
-  expect(versionPostgres).not.toBeNull();
+      //Validando versão do Postgres
+      const versionPostgres = responseBody.dependencies.version;
+      expect(versionPostgres).toEqual("16.0");
+      expect(versionPostgres).not.toBeNull();
 
-  //Validando quantidade maxima de conexões ao banco
-  const maxconnections = responseBody.dependencies.max_connections;
-  expect(maxconnections).toEqual(100);
-  expect(maxconnections).not.toBeNull();
+      //Validando quantidade maxima de conexões ao banco
+      const maxconnections = responseBody.dependencies.max_connections;
+      expect(maxconnections).toEqual(100);
+      expect(maxconnections).not.toBeNull();
 
-  //Valida conexão existente
-  expect(responseBody.dependencies.active_connections).toBeDefined();
-  expect(responseBody.dependencies.active_connections).toEqual(1);
+      //Valida conexão existente
+      expect(responseBody.dependencies.active_connections).toBeDefined();
+      expect(responseBody.dependencies.active_connections).toEqual(1);
+    });
+  });
 });
